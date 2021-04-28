e06417f1c92c6417aebe99084e0bb0e4e9576de6
tmushayahama <tmushayahama@gmail.com>
2021-04-27 23:24:02 -0700

    annoq test environment. port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   dataset: 'annoq-test',
-  annotationApi: 'http://bioghost2.usc.edu:3404',
+  annotationApi: 'http://bioghost2.usc.edu:3403',
   snpResultsSize: 50
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   dataset: 'annoq-test',
   production: false,
-  annotationApi: 'http://bioghost2.usc.edu:3404',//'http://68.181.46.17:3404/annoq-test', //'http://bioghost.usc.edu:5000',
+  annotationApi: 'http://bioghost2.usc.edu:3403',//'http://68.181.46.17:3403/annoq-test', //'http://bioghost.usc.edu:5000',
   snpResultsSize: 50
 };
 
