6d9b5687106077204237049975f09a213a73b8b4
Anushya Muruganujan <muruganu@usc.edu>
2026-02-13 14:52:14 -0800

    For #/issues/19

--- graphql_codegen.ts
+++ graphql_codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://topmed.api-v2.annoq.org/graphql",
+  schema: "  schema: "http://localhost:8001/graphql",
   generates: {
     "src/generated/graphql.ts": {
       plugins: ["typescript"]
